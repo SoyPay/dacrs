dc9161afd04f8a860f9118ade43380a1a0def4a3
franklin <flin1866@gmail.com>
2014-11-04 15:47:08 +0800

    modify scriptdb interface
    Former-commit-id: 66a0fc37b9f0ed30adedee04019116a9ea8a1718

--- src/test/scriptdb_tests.cpp
+++ src/test/scriptdb_tests.cpp
@@ -1,9 +1,9 @@
-#include "rpcclient.h"
-#include "util.h"
-#include <boost/test/unit_test.hpp>
+#include "main.h"
 #include "txdb.h"
 #include "account.h"
 #include <iostream>
+#include <boost/test/unit_test.hpp>
+
 using namespace std;
 
 void init() {
@@ -11,27 +11,88 @@ void init() {
 	 pScriptDBTip = new CScriptDBViewCache(*pScriptDB, false);
 }
 
-
-void ReadData() {
-
+void testscriptdb() {
+	vector<unsigned char> vScriptId = {0x01,0x00,0x00,0x00,0x01,0x00};
+	vector<unsigned char> vScriptContent = {0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01};
+	//write script content to db
+	BOOST_CHECK(pScriptDBTip->SetScript(vScriptId, vScriptContent));
+	//write all data in caches to db
+	BOOST_CHECK(pScriptDBTip->Flush());
+	//test if the script id is exist in db
+	BOOST_CHECK(pScriptDBTip->HaveScript(vScriptId));
+	vector<unsigned char> vScript;
+	//read script content from db by scriptId
+	BOOST_CHECK(pScriptDBTip->GetScript(vScriptId,vScript));
+	// if the readed script content equals with original
+	BOOST_CHECK(vScriptContent == vScript);
+	int nCount;
+	//get script numbers from db
+	BOOST_CHECK(pScriptDBTip->GetScriptCount(vScriptId, nCount));
+	//if the number is one
+	BOOST_CHECK_EQUAL(nCount, 1);
+	//get index 0 script from db
+	vScript.clear();
+	BOOST_CHECK(pScriptDBTip->GetScript(0,vScript));
+	//delete script from db
+	BOOST_CHECK(pScriptDBTip->EraseScript(vScriptId));
+	BOOST_CHECK(pScriptDBTip->GetScriptCount(vScriptId, nCount));
+	BOOST_CHECK_EQUAL(nCount, 0);
+	//write all data in caches to db
+	BOOST_CHECK(pScriptDBTip->Flush());
 }
 
-void WriteData() {
-	vector<unsigned char> vScriptId();
+
+void testscriptdatadb() {
+	vector<unsigned char> vScriptId = {0x01,0x00,0x00,0x00,0x02,0x00};
+	vector<unsigned char> vScriptKey = {0x01,0x00,0x02,0x03,0x04,0x05,0x06,0x07};
+	vector<unsigned char> vScriptKey1 = {0x01,0x00,0x02,0x03,0x04,0x05,0x07,0x06};
+	vector<unsigned char> vScriptData = {0x01,0x01,0x01,0x01,0x01};
+	vector<unsigned char> vScriptData1 = {0x01,0x01,0x01,0x00,0x00};
+	//write script data to db
+	BOOST_CHECK(pScriptDBTip->SetScriptData(vScriptId, vScriptKey, vScriptData, 100));
+	BOOST_CHECK(pScriptDBTip->SetScriptData(vScriptId, vScriptKey1, vScriptData1, 101));
+	//write all data in caches to db
+	BOOST_CHECK(pScriptDBTip->Flush());
+	//test if the script id is exist in db
+	BOOST_CHECK(pScriptDBTip->HaveScriptData(vScriptId, vScriptKey));
+	vector<unsigned char> vScript;
+	int nValidHeight;
+	//read script content from db by scriptId
+	BOOST_CHECK(pScriptDBTip->GetScriptData(vScriptId,vScriptKey,vScript,nValidHeight));
+	// if the readed script content equals with original
+	BOOST_CHECK(vScriptData == vScript);
+	BOOST_CHECK_EQUAL(nValidHeight, 100);
+	int nCount;
+	//get script numbers from db
+	BOOST_CHECK(pScriptDBTip->GetScriptDataCount(vScriptId, nCount));
+	//if the number is one
+	BOOST_CHECK_EQUAL(nCount, 2);
+	//get index 0 script from db
+	vScript.clear();
+	vector<unsigned char> vKey;
+	vKey.clear();
+	nValidHeight = 0;
+	BOOST_CHECK(pScriptDBTip->GetScriptData(vScriptId,0,vKey,vScript,nValidHeight));
+	BOOST_CHECK(vKey == vScriptKey);
+	BOOST_CHECK(vScript == vScriptData);
+	BOOST_CHECK_EQUAL(nValidHeight,100);
+	BOOST_CHECK(pScriptDBTip->GetScriptData(vScriptId,1,vKey,vScript,nValidHeight));
+	BOOST_CHECK(vKey == vScriptKey1);
+	BOOST_CHECK(vScript == vScriptData1);
+	BOOST_CHECK_EQUAL(nValidHeight,101);
+	//delete script from db
+	BOOST_CHECK(pScriptDBTip->EraseScriptData(vScriptId, vScriptKey));
+	BOOST_CHECK(pScriptDBTip->GetScriptDataCount(vScriptId, nCount));
+	BOOST_CHECK_EQUAL(nCount, 1);
+	//write all data in caches to db
+	BOOST_CHECK(pScriptDBTip->Flush());
 }
-
-void TraversalData() {
-
-}
-
-
-
 
 BOOST_AUTO_TEST_SUITE(scriptdb_test)
 BOOST_AUTO_TEST_CASE(test)
 {
-	WriteData();
-	ReadData();
-	TraversalData();
+	init();
+	testscriptdb();
+	testscriptdatadb();
 }
 BOOST_AUTO_TEST_SUITE_END()
