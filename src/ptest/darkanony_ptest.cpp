230a543135b128654b59764eb2af4bca193a4f7b
bess <776536184@qq.com>
2014-12-02 18:08:53 +0800

    add test code
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: d802c5be84f7e5dc855ce679938506d66c98f6f0

---
+++ src/ptest/darkanony_ptest.cpp
@@ -0,0 +1,283 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+#include "miner.h"
+#include "uint256.h"
+#include "util.h"
+#include <boost/foreach.hpp>
+#include <boost/test/unit_test.hpp>
+#include "VmScript/VmScriptRun.h"
+#include "VmScript/CVir8051.h"
+#include "VmScript/TestMcu.h"
+#include "json/json_spirit_writer_template.h"
+#include "rpcclient.h"
+#include "json/json_spirit_reader_template.h"
+#include "json/json_spirit_reader.h"
+#include "json/json_spirit_writer.h"
+#include "json/json_spirit_value.h"
+#include "json/json_spirit_stream_reader.h"
+using namespace std;
+using namespace boost;
+using namespace json_spirit;
+
+extern Object CallRPC(const string& strMethod, const Array& params);
+extern int TestCallRPC(std::string strMethod, const std::vector<std::string> &vParams, std::string &strRet);
+extern void GetAccountInfo(char *address);
+extern void GenerateMiner();
+
+void CreateScript(char * vmpath)
+{
+	int argc = 7;
+	char *argv[7] =
+			{ "rpctest", "registerscripttx", "mvVp2PDRuG4JJh6UjkJFzXUC8K5JVbMFFA","0",
+					vmpath,
+					"1000000", "2" };
+	CommandLineRPC(argc, argv);
+}
+typedef struct  {
+	unsigned char nType;					//!<类型
+	char buyer[6];						//!<买家ID（采用6字节的账户ID）
+	char seller[6];						//!<卖家ID（采用6字节的账户ID）
+	int nHeight;							//!<超时绝对高度
+	uint64_t nPayMoney;						//!<买家向卖家支付的金额
+	IMPLEMENT_SERIALIZE
+	(
+			READWRITE(nType);
+			for(int i = 0;i < 6;i++)
+			READWRITE(buyer[i]);
+			for(int i = 0;i < 6;i++)
+			READWRITE(seller[i]);
+			READWRITE(nHeight);
+			READWRITE(nPayMoney);
+	)
+} FIRST_CONTRACT;
+
+typedef struct {
+	unsigned char nType;				//!<交易类型
+	unsigned char hash[32];		//!<上一个交易包的哈希
+	IMPLEMENT_SERIALIZE
+	(
+			READWRITE(nType);
+			for(int i = 0;i < 32;i++)
+			READWRITE(hash[i]);
+	)
+} NEXT_CONTRACT;
+string Parsejson(string str)
+{
+	json_spirit::Value val;
+	json_spirit::read(str, val);
+//	json_spirit::json ::
+	if (val.type() != obj_type)
+	{
+		return "";
+	}
+	json_spirit::Value::Object obj=  val.get_obj();
+	string ret;
+	for(int i = 0; i < obj.size(); ++i)
+	{
+		const json_spirit::Pair& pair = obj[i];
+		const std::string& str_name = pair.name_;
+		const json_spirit::Value& val_val = pair.value_;
+		if(str_name =="RawTx")
+		{
+			if(val_val.get_str() != "")
+			{
+				ret = val_val.get_str();
+			}
+		}else if(str_name =="txhash")
+		{
+			ret = val_val.get_str();
+		}
+
+	}
+	return ret;
+}
+string CreateDarkTx()
+{
+	string accountid = "010000000100";
+	int argc = 8;
+	std::vector<std::string> vInputParams;
+	vInputParams.clear();
+	vInputParams.push_back("010000000100");
+	vInputParams.push_back(
+			"[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\",\"mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y\"]");
+
+	FIRST_CONTRACT contact;
+	contact.nType = 1;
+	contact.nHeight = 10;
+	contact.nPayMoney = 100;
+	CRegID regIdb("0-5");
+	CRegID regIds("0-3");
+	memcpy(contact.buyer,&regIdb.GetVec6().at(0),sizeof(contact.buyer));
+	memcpy(contact.seller,&regIds.GetVec6().at(0),sizeof(contact.seller));
+	CDataStream scriptData(SER_DISK, CLIENT_VERSION);
+	scriptData << contact;
+	string temp = HexStr(scriptData);
+	vInputParams.push_back(temp);
+	vInputParams.push_back("1000000");
+	vInputParams.push_back("10");
+	std::string strReturn("");
+	if(TestCallRPC("createcontracttx", vInputParams, strReturn)){
+		cout<<strReturn<<endl;
+			strReturn= Parsejson(strReturn);
+			cout<<strReturn<<endl;
+			vInputParams.clear();
+			vInputParams.push_back(strReturn);
+			if (TestCallRPC("signcontracttx", vInputParams, strReturn) > 0) {
+				 strReturn= Parsejson(strReturn);
+				cout<<strReturn<<endl;
+				vInputParams.clear();
+				vInputParams.push_back(strReturn);
+				//cout <<"sign securte tx succeed" << endl;
+				if (TestCallRPC("signcontracttx", vInputParams, strReturn) > 0) {
+					cout<<"sucess:"<<strReturn<<endl;
+					return strReturn;
+				}
+			}
+		}
+	cout<<strReturn<<endl;
+	return strReturn;
+}
+void CreateSecondDarkTx(string hash)
+{
+	string accountid = "010000000100";
+	int argc = 8;
+	std::vector<std::string> vInputParams;
+	vInputParams.clear();
+	vInputParams.push_back("010000000100");
+	vInputParams.push_back(
+			"[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\"]");
+
+	NEXT_CONTRACT contact;
+	contact.nType = 2;
+	uint160 tx(hash);
+	memcpy(contact.hash,tx.begin(),sizeof(contact.hash));
+	CDataStream scriptData(SER_DISK, CLIENT_VERSION);
+	scriptData << contact;
+	string temp = HexStr(scriptData);
+	vInputParams.push_back(temp);
+	vInputParams.push_back("1000000");
+	vInputParams.push_back("10");
+	std::string strReturn("");
+	TestCallRPC("createcontracttx", vInputParams, strReturn);
+	cout<<strReturn<<endl;
+	return ;
+}
+
+typedef struct  {
+	char 	Sender[6];						//!<转账人ID（采用6字节的账户ID）
+	int nHeight;							//!<超时绝对高度
+	uint64_t nPayMoney;						//!<转账的人支付的金额
+	unsigned short len;                     //!<接受钱账户信息长度
+	IMPLEMENT_SERIALIZE
+	(
+			for(int i = 0;i < 6;i++)
+			READWRITE(Sender[i]);
+			READWRITE(nHeight);
+			READWRITE(nPayMoney);
+			READWRITE(len);
+	)
+}CONTRACT_ANONY;
+
+typedef struct  {
+	char 	account[6];						//!<接受钱的ID（采用6字节的账户ID）
+	uint64_t nReciMoney;						    //!<	收到钱的金额
+	IMPLEMENT_SERIALIZE
+	(
+			for(int i = 0;i < 6;i++)
+			READWRITE(account[i]);
+			READWRITE(nReciMoney);
+
+	)
+} ACCOUNT_INFO;
+
+void Createanony(string addr)
+{
+	string accountid = "010000000100";
+	int argc = 8;
+	std::vector<std::string> vInputParams;
+	vInputParams.clear();
+	vInputParams.push_back("010000000100");
+	string temp1 = "[";
+	temp1+="\""+addr+"\""+"]";
+	cout<<temp1<<endl;
+	vInputParams.push_back(temp1);
+
+	string temp = "";
+	CONTRACT_ANONY contact;
+	contact.nHeight = 10;
+	contact.nPayMoney = 100;
+
+
+	CRegID regIdb("0-5");
+	memcpy(contact.Sender,&regIdb.GetVec6().at(0),sizeof(contact.Sender));
+
+	ACCOUNT_INFO info;
+	info.nReciMoney = 50;
+	CRegID regId1("0-3");
+	memcpy(info.account,&regId1.GetVec6().at(0),sizeof(info.account));
+
+	ACCOUNT_INFO info1;
+	info1.nReciMoney = 50;
+	CRegID regId2("0-8");
+	memcpy(info1.account,&regId2.GetVec6().at(0),sizeof(info1.account));
+
+	contact.len = ::GetSerializeSize(info, SER_DISK, CLIENT_VERSION)*2;
+//	cout<<contact.len<<endl;
+	CDataStream scriptData(SER_DISK, CLIENT_VERSION);
+	scriptData << contact;
+	scriptData << info;
+	scriptData << info1;
+	temp = HexStr(scriptData);
+//	cout<<"cotx:"<<temp<<endl;
+	vInputParams.push_back(temp);
+	vInputParams.push_back("1000000");
+	vInputParams.push_back("10");
+	std::string strReturn("");
+	TestCallRPC("createcontracttx", vInputParams, strReturn);
+	cout<<strReturn<<endl;
+	return ;
+}
+BOOST_AUTO_TEST_SUITE(test_app)
+
+BOOST_AUTO_TEST_CASE(test_dark){
+	CreateScript("D:\\cppwork\\vmsdk\\darksecure\\Debug\\Exe\\darksecure.bin");
+	GenerateMiner();
+	cout<<"1"<<endl;
+	string temp = CreateDarkTx();
+	GenerateMiner();
+//	GetAccountInfo("010000000100");
+//	GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+//	GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
+//	cout<<"2"<<endl;
+//	CreateSecondDarkTx(temp);
+//	GenerateMiner();
+//	GetAccountInfo("010000000100");
+//	GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+//	GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
+}
+
+BOOST_AUTO_TEST_CASE(test_anony){
+
+
+	CreateScript("D:\\cppwork\\vmsdk\\anonymous\\Debug\\Exe\\anony.bin");
+	GenerateMiner();
+	cout<<"1"<<endl;
+	Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	GenerateMiner();
+	GetAccountInfo("010000000100");
+	GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	cout<<"2"<<endl;
+	Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	GenerateMiner();
+	GetAccountInfo("010000000100");
+	GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	cout<<"3"<<endl;
+	Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	GenerateMiner();
+	GetAccountInfo("010000000100");
+	GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
+	GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
+	GetAccountInfo("n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
+}
+BOOST_AUTO_TEST_SUITE_END()
