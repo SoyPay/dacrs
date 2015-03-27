fffd8d69f6f1f6891ab2e61673682fb96f118e8e
ranger.shi <ranger.shi@gmail.com>
2015-03-27 15:10:26 +0800

    add CAppUserAccout
    Signed-off-by: ranger.shi <ranger.shi@gmail.com>
    
    Former-commit-id: aca348b5eeb2e78fdeabfd76527a5087cca4b3de [formerly aca348b5eeb2e78fdeabfd76527a5087cca4b3de [formerly aca348b5eeb2e78fdeabfd76527a5087cca4b3de [formerly 8f2c120fc953f403a8a1035ef167560a48a7a094]]]
    Former-commit-id: 466980db6cc8349d726ad0a1a342ec8beaa18bd1
    Former-commit-id: a2b8405b3ce07516aebfae01ea5ed80717bb8c58 [formerly fbc09874ca14b17629c762e157d311a6a6d6933e]
    Former-commit-id: 4e8d30b98d1c4dcf65ab85ab74dbb07bbeb7a29f

--- src/tx.h
+++ src/tx.h
@@ -1021,6 +1021,68 @@ private:
 	uint64_t GetVecMoney(const vector<CFund>& vFund);
 };
 
+
+class CAppCFund {
+public:
+	CAppCFund();
+	CAppCFund(const CAppCFund &fund);
+	CAppCFund(const vector<unsigned char> &vtag,uint64_t val,int nhight);
+	bool MergeCFund( const CAppCFund &fund);
+	const vector<unsigned char> GetTag() const {
+		return vTag;
+	}
+
+	int getheight() const {
+		return nHeight;
+	}
+
+	void setHeight(int height) {
+		nHeight = height;
+	}
+
+	uint64_t getvalue() const {
+		return value;
+	}
+
+	void setValue(uint64_t value) {
+		this->value = value;
+	}
+
+	const vector<unsigned char>& gettag() const {
+		return vTag;
+	}
+
+	void setTag(const vector<unsigned char>& tag) {
+		vTag = tag;
+	}
+
+public:
+
+private:
+	uint64_t value;					//!< amount of money
+	int nHeight;					//!< time-out height
+	vector<unsigned char> vTag;	//!< vTag of the tx which create the fund
+
+};
+
+class CAppUserAccout {
+public:
+	uint64_t llValues;
+	CRegID regID;
+	vector<CAppCFund> vFreezedFund;
+	bool GetAppCFund(CAppCFund &outFound,const vector<unsigned char> &vtag);
+	bool AddAppCFund(const CAppCFund &inFound);
+	bool AddAppCFund(const vector<unsigned char> &vtag,uint64_t val,int nhight);
+	bool MinusAppCFund(const vector<unsigned char> &vtag,uint64_t val,int nhight);
+	bool MinusAppCFund(const CAppCFund &inFound);
+	bool ChangeAppCFund(const CAppCFund &inFound);
+	bool AutoMergeFreezeToFree(int hight);
+	CAppUserAccout();
+	virtual ~CAppUserAccout();
+};
+
+
+
 inline unsigned int GetSerializeSize(const std::shared_ptr<CBaseTransaction> &pa, int nType, int nVersion) {
 	return pa->GetSerializeSize(nType, nVersion) + 1;
 }
@@ -1079,5 +1141,7 @@ void Unserialize(Stream& is, std::shared_ptr<CBaseTransaction> &pa, int nType, i
 	}
 	pa->nTxType = nTxType;
 }
+
+
 
 #endif
