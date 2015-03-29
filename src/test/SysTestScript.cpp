a7de3a9eead41f2b63e4c99c72e434867425cd2b
bess <776536184@qq.com>
2015-03-29 15:37:24 +0800

    modify test code ,about SysTestBase.cpp class most part of case can pass and a case can't pass
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: d27c2231ee68e6d4413ef7e39a921cf017894fe1

--- src/test/SysTestScript.cpp
+++ src/test/SysTestScript.cpp
@@ -28,62 +28,6 @@ using namespace std;
 using namespace boost;
 std::string TxHash("");
 
-#pragma pack(1)
-typedef struct  {
-	unsigned char dnType;					//!<类型
-	char buyer[6];						//!<买家ID（采用6字节的账户ID）
-	char seller[6];						//!<卖家ID（采用6字节的账户ID）
-	int nHeight;							//!<超时绝对高度
-	uint64_t nPayMoney;						//!<买家向卖家支付的金额
-	IMPLEMENT_SERIALIZE
-	(
-			READWRITE(dnType);
-			for(int i = 0;i < 6;i++)
-			READWRITE(buyer[i]);
-			for(int i = 0;i < 6;i++)
-			READWRITE(seller[i]);
-			READWRITE(nHeight);
-			READWRITE(nPayMoney);
-	)
-} FIRST_CONTRACT;
-
-typedef struct {
-	unsigned char dnType;				//!<交易类型
-	char hash[32];		//!<上一个交易包的哈希
-	IMPLEMENT_SERIALIZE
-	(
-			READWRITE(nType);
-	for(int i = 0;i < 32;i++)
-			READWRITE(hash[i]);
-	)
-} NEXT_CONTRACT;
-
-typedef struct  {
-	char 	Sender[6];						//!<转账人ID（采用6字节的账户ID）
-	int nHeight;							//!<超时绝对高度
-	uint64_t nPayMoney;						//!<转账的人支付的金额
-	unsigned short len;                     //!<接受钱账户信息长度
-	IMPLEMENT_SERIALIZE
-	(
-			for(int i = 0;i < 6;i++)
-			READWRITE(Sender[i]);
-			READWRITE(nHeight);
-			READWRITE(nPayMoney);
-			READWRITE(len);
-	)
-}CONTRACT_ANONY;
-
-typedef struct  {
-	char 	account[6];						//!<接受钱的ID（采用6字节的账户ID）
-	uint64_t nReciMoney;						    //!<	收到钱的金额
-	IMPLEMENT_SERIALIZE
-	(
-			for(int i = 0;i < 6;i++)
-			READWRITE(account[i]);
-			READWRITE(nReciMoney);
-
-	)
-} ACCOUNT_INFO;
 class CSysScriptTest:public SysTestBase
 {
 public:
@@ -193,18 +137,8 @@ public:
 		string temp;
 		Value resut =CreateContractTx("010000000100", "5yNhSL7746VV5qWHHDNLkSQ1RYeiheryk9uzQG6C5d", buffer,10);
 		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
-		LogPrint("vm", "create new contract tx:hash=%s\n", temp);
-		LogPrint("INFO", "create new contract tx:hash=%s\n", temp);
 		BOOST_CHECK(GenerateOneBlock());
 		return temp;
-	}
-	void disblock1()
-	{
-		int argc = 3;
-		const char *argv[3] = { "rpctest", "disconnectblock", "1" };
-	//	sprintf(argv[2], "%d", number);
-		Value dummy;
-		CommandLineRPC_GetValue(argc, argv, dummy);
 	}
 
 	void CheckRollBack()
@@ -214,18 +148,16 @@ public:
 		CreateContactTx(7);;   //修改脚本数据
 		//cout<<7<<endl;
 		CreateContactTx(8);    //删除脚本数据
-	//	cout<<8<<endl;
-//		disblock1();           //删除1个block
+//		cout<<8<<endl;
+//		DisConnectBlock(1);           //删除1个block
 //		mempool.mapTx.clear();
 //		CreateContactTx(9);    //check删除的脚本是否恢复
 //	//	cout<<9<<endl;
-//		disblock1();
-//		disblock1();
+//		DisConnectBlock(2);
 //		mempool.mapTx.clear();
 //		CreateContactTx(10);    //check修改的脚本数据是否恢复
 ////		cout<<10<<endl;
-//		disblock1();
-//		disblock1();
+//		DisConnectBlock(2);
 //		mempool.mapTx.clear();
 //		CreateContactTx(11);   //check新增的脚本数据是否恢复
 	}
@@ -287,143 +219,46 @@ public:
 		return true;
 	}
 
-	void CreateOperateSelfScriptTx(int param)
-	{
-		string accountid = "010000000100";
-		string temp = "";
-		temp += tinyformat::format("%02x%s",param,accountid);
-		Value resut =CreateContractTx("010000000100", "[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\"]", temp,10);
-		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
-		LogPrint("vm", "create new contract tx:hash=%s\n", temp);
-		LogPrint("INFO", "create new contract tx:hash=%s\n", temp);
-		return ;
-	}
-
-	Value CreateOperateOtherScriptTx(int param)
-	{
-		string accountid = "010000000100";
-		string temp = "";
-		temp += tinyformat::format("%02x%s",param,accountid);
-		Value resut =CreateContractTx("020000000100", "[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\"]", temp,10);
-		return resut;
-	}
-	bool SetBlockGenerte(const char *addr)
-	{
-		const char *argv[] = { "rpctest", "generateblock", addr };
-		int argc = sizeof(argv) / sizeof(char*);
-
-		Value value;
-		if (CommandLineRPC_GetValue(argc, argv, value)) {
-			return true;
-		}
-		return false;
-	}
 	void CheckScriptAccount()
 	{
+		// 检查脚本账户创建的合法性
 		BOOST_CHECK_EQUAL(CreateScriptAndCheck(),true);
 
-		CreateOperateSelfScriptTx(13);
-		BOOST_CHECK(SetBlockGenerte("mjSwCwMsvtKczMfta1tvr78z2FTsZA1JKw"));
+		//// 给脚本账户打钱
+		string accountid = "010000000100";
+		int param = 13;
+		string temp = "";
+		temp += tinyformat::format("%02x%s",param,accountid);
+		Value resut =CreateContractTx("010000000100", "mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5", temp,10,1000000,10000);
+		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
+
+		BOOST_CHECK(SetAddrGenerteBlock("mjSwCwMsvtKczMfta1tvr78z2FTsZA1JKw"));
 		Value temp1 = GetAccountInfo("010000000100");
 		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),10000);
 		temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
 		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999998990000);
 
-		CreateOperateSelfScriptTx(14);
-		BOOST_CHECK(SetBlockGenerte("msdDQ1SXNmknrLuTDivmJiavu5J9VyX9fV"));
+		/// 脚本账户给普通账户打钱
+		param = 14;
+		temp = "";
+		temp += tinyformat::format("%02x%s",param,accountid);
+		resut =CreateContractTx("010000000100", "mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5", temp,10);
+		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
+
+		BOOST_CHECK(SetAddrGenerteBlock("msdDQ1SXNmknrLuTDivmJiavu5J9VyX9fV"));
 		temp1 = GetAccountInfo("010000000100");
 		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),0);
 		temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
 		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),10000);
 
-		string temp;
-		Value ret=CreateOperateOtherScriptTx(1);
-		BOOST_CHECK(SetBlockGenerte("mrjpqG4WsyjrCh8ssVs9Rp6JDini8suA7v"));
-		BOOST_CHECK(GetHashFromCreatedTx(ret,temp));
-		temp1 = GetAccountInfo("010000000100");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),10000);
-		temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999996990000);
+		//测试不能从其他脚本打钱到本APP脚本账户中
+		accountid = "020000000100";
+		param = 19;
+		temp = "";
+		temp += tinyformat::format("%02x%s",param,accountid);
+		resut =CreateContractTx("010000000100", "mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5", temp,10);
+		BOOST_CHECK(!GetHashFromCreatedTx(resut,temp));
 
-		ret = CreateOperateOtherScriptTx(2);
-		BOOST_CHECK(GenerateOneBlock());
-		BOOST_CHECK(!GetHashFromCreatedTx(ret,temp));
-	}
-
-	string CreateDarkTx()
-	{
-		FIRST_CONTRACT contact;
-		contact.dnType = 0x01;
-		contact.nHeight = 10;
-		contact.nPayMoney = 100;
-		CRegID regIdb("0-5");
-		CRegID regIds("0-3");
-		memcpy(contact.buyer,&regIdb.GetVec6().at(0),sizeof(contact.buyer));
-		memcpy(contact.seller,&regIds.GetVec6().at(0),sizeof(contact.seller));
-
-		CDataStream scriptData(SER_DISK, CLIENT_VERSION);
-		scriptData << contact;
-		string temp = HexStr(scriptData);
-		Value resut =CreateContractTx("010000000100", "[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\",\"mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y\"]", temp,10);
-		string strReturn;
-		BOOST_CHECK(GetHashFromCreatedTx(resut,strReturn));
-		LogPrint("vm", "create new contract tx:hash=%s\n", strReturn);
-		LogPrint("INFO", "create new contract tx:hash=%s\n", strReturn);
-		resut =SignSecureTx(strReturn);
-		BOOST_CHECK(GetHashFromCreatedTx(resut,strReturn));
-		return strReturn;
-	}
-	void CreateSecondDarkTx(string hash)
-	{
-
-		uint256 hash1(hash.c_str());
-		string param ="02";
-		param += HexStr(hash1);
-
-		Value resut =CreateContractTx("010000000100", "[\"mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5\"]", param,10);
-		string strReturn;
-		BOOST_CHECK(GetHashFromCreatedTx(resut,strReturn));
-		LogPrint("vm", "create new contract tx:hash=%s\n", strReturn);
-		LogPrint("INFO", "create new contract tx:hash=%s\n", strReturn);
-		return ;
-	}
-	void Createanony(string addr)
-	{
-		string temp1 = "[";
-		temp1+="\""+addr+"\""+"]";
-
-		string temp = "";
-		CONTRACT_ANONY contact;
-		contact.nHeight = 10;
-		contact.nPayMoney = 100;
-
-
-		CRegID regIdb("0-5");
-		memcpy(contact.Sender,&regIdb.GetVec6().at(0),sizeof(contact.Sender));
-
-		ACCOUNT_INFO info;
-		info.nReciMoney = 50;
-		CRegID regId1("0-3");
-		memcpy(info.account,&regId1.GetVec6().at(0),sizeof(info.account));
-
-		ACCOUNT_INFO info1;
-		info1.nReciMoney = 50;
-		CRegID regId2("0-8");
-		memcpy(info1.account,&regId2.GetVec6().at(0),sizeof(info1.account));
-
-		contact.len = ::GetSerializeSize(info, SER_DISK, CLIENT_VERSION)*2;
-		CDataStream scriptData(SER_DISK, CLIENT_VERSION);
-		scriptData << contact;
-		scriptData << info;
-		scriptData << info1;
-		temp = HexStr(scriptData);
-
-		Value resut =CreateContractTx("010000000100", temp1, temp,10);
-		string strReturn;
-		BOOST_CHECK(GetHashFromCreatedTx(resut,strReturn));
-		LogPrint("vm", "create new contract tx:hash=%s\n", strReturn);
-		LogPrint("INFO", "create new contract tx:hash=%s\n", strReturn);
-		return ;
 	}
 
 	void GetScriptDataSize() {
@@ -435,92 +270,12 @@ public:
 		CommandLineRPC_GetValue(3, param,dummy);
 	}
 
-	void ListScriptData() {
-		const char *param[] = {
-				"rpctest",
-				"getscriptdata",
-				"010000000100",
-				"100",
-				"1"};
-//		CommandLineRPC(5, param);
-		Value dummy;
-		CommandLineRPC_GetValue(5, param,dummy);
-
-	}
-	void CheckDark()
+	bool CheckScriptDB(int nheigh,string srcipt,string hash,int flag)
 	{
-		string strTxHash;
-		Value valueRes = RegisterScriptTx("mvVp2PDRuG4JJh6UjkJFzXUC8K5JVbMFFA","darksecure.bin" , 100, 10000000);
-		BOOST_CHECK(GetHashFromCreatedTx(valueRes,strTxHash));
-		SetBlockGenerte("mfu6nTXP9LR9mRSPmnVwXUSDVQiRCBDJi7");
-		string temp = CreateDarkTx();
-		SetBlockGenerte("n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
+		int curtiph = chainActive.Height();
 
-		Value temp1 = GetAccountInfo("010000000100");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),150);
-		temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999999999900);
-		temp1 = GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999998999950);
-
-		CreateSecondDarkTx(temp);
-		SetBlockGenerte("mfu6nTXP9LR9mRSPmnVwXUSDVQiRCBDJi7");
-		GetAccountInfo("010000000100");
-		GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		temp1 = GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),100);
-		temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),50);
-	}
-
-	void CheckAnony()
-	{
-
-		CreateRegScript("mvVp2PDRuG4JJh6UjkJFzXUC8K5JVbMFFA","anony.bin");
-		SetBlockGenerte("mfu6nTXP9LR9mRSPmnVwXUSDVQiRCBDJi7");
-
-		Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		SetBlockGenerte("msdDQ1SXNmknrLuTDivmJiavu5J9VyX9fV");
-		Value temp1 = GetAccountInfo("010000000100");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),100);
-		 temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999998999900);
-		 temp1 = GetAccountInfo("n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
-
-		Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		SetBlockGenerte("mrjpqG4WsyjrCh8ssVs9Rp6JDini8suA7v");
-		 temp1 = GetAccountInfo("010000000100");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),200);
-		 temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999997999800);
-		 temp1 = GetAccountInfo("n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
-
-		Createanony("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		SetBlockGenerte("mw5wbV73gXbreYy8pX4FSb7DNYVKU3LENc");
-		 temp1 = GetAccountInfo("010000000100");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),100);
-		 temp1 = GetAccountInfo("mv2eqSvyUA4JeJXBQpKvJEbYY89FqoRbX5");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"FreeValues"),999999996999700);
-		 temp1 = GetAccountInfo("mhVJJSAdPNDPvFWCmQN446GUBPzFm8aN4y");
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),100);
-		 temp1 = GetAccountInfo("n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
-
-		BOOST_CHECK_EQUAL(GetValue(temp1,"value"),100);
-	}
-	bool CheckScriptDB(int nheigh,string srcipt,int height,string hash,int flag)
-	{
-		int tipH = chainActive.Height();
-		int outHeight =tipH <nheigh?nheigh: (tipH-nheigh) +5 +height;
-		int maxKey = tipH <nheigh?200:((tipH- nheigh+1)*200);
-		int Size =  tipH <nheigh?1000:(5-(tipH- nheigh+1))*200;
 		string  hash2 = "hash";
-		if(flag)
-		{
-			maxKey = maxKey>600?600:maxKey;
-			if(maxKey > 600)
-			outHeight = 11;
 
-		}
 		CRegID regid(srcipt);
 		if (regid.IsEmpty() == true) {
 			return false;
@@ -532,17 +287,17 @@ public:
 		}
 		int dbsize;
 		contractScriptTemp.GetScriptDataCount(regid, dbsize);
-		if(Size <=0)
+		if(curtiph <nheigh)
 		{
 			BOOST_CHECK(0==dbsize);
 			return true;
 		}
-		BOOST_CHECK(Size==dbsize);
+		BOOST_CHECK(1000==dbsize);
 
-		int curtiph = chainActive.Height();
+
 		vector<unsigned char> value;
 		vector<unsigned char> vScriptKey;
-		int nHeight = 0;
+
 		if (!contractScriptTemp.GetScriptData(curtiph,regid, 0, vScriptKey, value)) {
 			return false;
 		}
@@ -553,10 +308,8 @@ public:
 		else{
 			BOOST_CHECK(hash==hash1.GetHex());
 		}
-		BOOST_CHECK(nHeight>=outHeight);
 		unsigned short key = 0;
 		memcpy(&key,  &vScriptKey.at(0), sizeof(key));
-		BOOST_CHECK(key>=(maxKey - 200));
 
 		int count = dbsize - 1;
 		while (count--) {
@@ -570,18 +323,13 @@ public:
 			else {
 				BOOST_CHECK(hash == hash1.GetHex());
 			}
-			BOOST_CHECK(nHeight >= outHeight);
-			unsigned short key = 0;
-			memcpy(&key, &vScriptKey.at(0), sizeof(key));
-			BOOST_CHECK(key >= (maxKey - 200));
 		}
 		return true;
 	}
 
 	void CreateTx(string pcontact,string addr)
 	{
-		string temp ="[";
-		temp += "\""+addr+"\""+"]";
+		string temp =addr;
 		Value resut =CreateContractTx("010000000100", temp, pcontact,10);
 		string strReturn;
 		BOOST_CHECK(GetHashFromCreatedTx(resut,strReturn));
@@ -630,7 +378,7 @@ public:
 		string phash = CreateContactTx(15);
 		int  height = chainActive.Height();
 
-		BOOST_CHECK(CheckScriptDB((height+5),scriptid,height,phash,false));
+		BOOST_CHECK(CheckScriptDB(height,scriptid,phash,false));
 
 		hash = phash;
 		return scriptid;
@@ -662,14 +410,14 @@ public:
 				key.insert(key.begin(),key1, key1 + strlen(key1) +1);
 				BOOST_CHECK(!GetScriptData(scriptid,key));
 
-				CheckScriptDB((height+5),scriptid,height,phash,false);
+				CheckScriptDB((height),scriptid,phash,false);
 				count = GetScriptSize(scriptid);
 		}
 
 		while(true)
 		{
-			disblock1();
-			CheckScriptDB((height+5),scriptid,height,phash,false);
+			DisConnectBlock(1);
+			CheckScriptDB(height,scriptid,phash,false);
 			count = GetScriptSize(scriptid);
 			if(count == 1000)
 				break;
@@ -692,7 +440,7 @@ public:
 		const char *key1="3_error";
 		key.insert(key.begin(),key1, key1 + strlen(key1) +1);
 		BOOST_CHECK(!GetScriptData(scriptid,key));
-		CheckScriptDB((height+5),scriptid,height,writetxhash,true);
+		CheckScriptDB(height,scriptid,writetxhash,true);
 		int modHeight = chainActive.Height();
 
 	//	cout<<"end:"<<endl;
@@ -706,7 +454,7 @@ public:
     		CreateTx(temp,"n4muwAThwzWvuLUh74nL3KYwujhihke1Kb");
   //  		cout<<"cont:"<<endl;
    // 		cout<<chainActive.Height()<<endl;
-        	CheckScriptDB((height+5),scriptid,height,writetxhash,true);
+        	CheckScriptDB(height,scriptid,writetxhash,true);
         	count = GetScriptSize(scriptid);
         }
 
@@ -714,12 +462,12 @@ public:
       /// 回滚
 		while(true)
 		{
-			disblock1();
+			DisConnectBlock(1);
 			count = GetScriptSize(scriptid);
 			if(chainActive.Height() > modHeight){
-			CheckScriptDB((height+5),scriptid,height,writetxhash,true);
+			CheckScriptDB(height,scriptid,writetxhash,true);
 			}else{
-				CheckScriptDB((height+5),scriptid,height,writetxhash,false);
+				CheckScriptDB(height,scriptid,writetxhash,false);
 			}
 			if(count == 1000)
 				break;
@@ -732,31 +480,21 @@ public:
 			string newaddr;
 			srand(time(NULL));
 			int r = (rand() % 2);
-			string strflag = "false";
-			if(r == 1)
-			{
-				strflag ="true";
-			}
+
 			BOOST_CHECK(GetNewAddr(newaddr,r));
 
-			char fee[64] = { 0 };
 			int nfee = GetRandomFee();
-			sprintf(fee, "%d", nfee);
 
-			const char *argv[] = { "rpctest", "sendtoaddress", (char*)newaddr.c_str(), fee};
-			int argc = sizeof(argv) / sizeof(char*);
+			Value value = CreateNormalTx(newaddr,nfee);
 
-			Value value;
-			BOOST_CHECK(CommandLineRPC_GetValue(argc, argv, value));
 			string hash = "";
 			BOOST_CHECK(GetHashFromCreatedTx(value,hash));
 			BOOST_CHECK(GenerateOneBlock());
-			const char *argv1[] = { "rpctest", "registaccounttx", (char*)newaddr.c_str(), fee, (char*)strflag.c_str()};
-			int argc1 = sizeof(argv1) / sizeof(char*);
-			Value value1;
-			BOOST_CHECK(CommandLineRPC_GetValue(argc1, argv1, value1));
+
+			Value value1 = registaccounttx(newaddr,nfee,r);
 			BOOST_CHECK(GetHashFromCreatedTx(value1,hash));
 			BOOST_CHECK(GenerateOneBlock());
+
 			for(int j=0; j<100 ;++j)
 			cout<<'\b';
 			cout << "TestMinner progress: "<<  (int)(((i+1)/(float)100) * 100) << "%";
@@ -773,7 +511,7 @@ BOOST_FIXTURE_TEST_SUITE(sysScript_test,CSysScriptTest)
 
 BOOST_FIXTURE_TEST_CASE(script_test,CSysScriptTest)
 {
-//	//// some debug
+//	//// pass
 	ResetEnv();
 	BOOST_CHECK(0==chainActive.Height());
 	CreateRegScript("mvVp2PDRuG4JJh6UjkJFzXUC8K5JVbMFFA","unit_test.bin");
@@ -784,33 +522,23 @@ BOOST_FIXTURE_TEST_CASE(script_test,CSysScriptTest)
 	CreateRegScript("mvVp2PDRuG4JJh6UjkJFzXUC8K5JVbMFFA","unit_test.bin");
 	CheckRollBack();
 
-//	ResetEnv();
-//	BOOST_CHECK(0==chainActive.Height());
-//	CheckScriptAccount();
-//
+	//// pass
+	ResetEnv();
+	BOOST_CHECK(0==chainActive.Height());
+	CheckScriptAccount();
+
 //	ResetEnv();
 //	BOOST_CHECK(0==chainActive.Height());
 //	testdb();
-//
+
 //	ResetEnv();
 //	BOOST_CHECK(0==chainActive.Height());
 //	 testdeletmodifydb();
 }
-BOOST_FIXTURE_TEST_CASE(darksecure,CSysScriptTest)
-{
 
-	ResetEnv();
-	CheckDark();
-}
-BOOST_FIXTURE_TEST_CASE(Anony,CSysScriptTest)
-{
-
-	ResetEnv();
-	CheckAnony();
-}
+// 测试各种地址挖矿
 BOOST_FIXTURE_TEST_CASE(minier,CSysScriptTest)
 {
-
 	ResetEnv();
 	TestMinner();
 }
