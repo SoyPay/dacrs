3f2a538fda21f379e858c53c817e9d5c366b6434
bess <776536184@qq.com>
2015-03-30 14:44:04 +0800

    windows config
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: 12a9553587aa36c8f36ae45f8be13104e4a0acc2 [formerly 946321fa17d229225913efa2df035804680fb7f6] [formerly 946321fa17d229225913efa2df035804680fb7f6 [formerly 946321fa17d229225913efa2df035804680fb7f6 [formerly 78f8d14e9ffcd8e99e8f93ca9da137a2fad62e00]]]
    Former-commit-id: bedee32235e0b7ef63eae2499193c93e818b783c
    Former-commit-id: 62b4051074cb0411707d0b3c2817ae0a2ac786a6 [formerly f30b0bde04ad753b17efe3877038d99ff18cb960]
    Former-commit-id: 8b45b2b376652eba77826cbc76a357ffa8edcab1

--- autogen-dacrs-man.sh
+++ autogen-dacrs-man.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ $# = 0 ]; then	
+if [ $# == 0 ]; then	
   	echo -e "\033[40;33m"
 		echo warming  your had not inputed assemble model
 		echo "$PWD"
@@ -12,7 +12,7 @@ if [ $# = 0 ]; then
 		echo	autogen-dacrs-man ["dacrs-d|dacrs-cli|dacrs-test|dacrs-ptest"]
 	echo -e "\033[40;37m"
 		exit 1
-elif [ $# = 1 ]; then
+elif [ $# == 1 ]; then
 	case $1 in 
 		dacrs-d)
 		flag1=--with-daemon
@@ -33,7 +33,7 @@ elif [ $# = 1 ]; then
 		exit 1
 		;;
 	esac
-elif [ $# = 2 ]; then
+elif [ $# == 2 ]; then
 	case $1 in 
 		dacrs-d)
 		flag1=--with-daemon
@@ -74,7 +74,7 @@ elif [ $# = 2 ]; then
 		exit 1
 		;;
 	esac
-elif [ $# = 3 ]; then
+elif [ $# == 3 ]; then
 	case $1 in 
 		dacrs-d)
 		flag1=--with-daemon
@@ -135,7 +135,7 @@ elif [ $# = 3 ]; then
 		exit 1
 		;;
 	esac
-elif [ $# = 4 ]; then
+elif [ $# == 4 ]; then
 	case $1 in 
 		dacrs-d)
 		flag1=--with-daemon
@@ -228,7 +228,23 @@ srcdir="$(dirname $0)"
 cd "$srcdir"
 autoreconf --install --force
 
-CPPFLAGS="-std=c++11" \
+CPPFLAGS="-I/c/deps/boost_1_55_0 \
+-I/c/deps/db-4.8.30.NC/build_unix \
+-I/c/deps/openssl-1.0.1g/include \
+-I/c/deps \
+-I/c/deps/protobuf-2.5.0/src \
+-I/c/deps/libpng-1.6.9 \
+-I/c/deps/qrencode-3.4.3 \
+-std=c++11 \
+" \
+CXXFLAGS="-Wall" \
+LDFLAGS="-L/c/deps/boost_1_55_0/stage/lib \
+-L/c/deps/db-4.8.30.NC/build_unix \
+-L/c/deps/openssl-1.0.1g \
+-L/c/deps/miniupnpc \
+-L/c/deps/protobuf-2.5.0/src/.libs \
+-L/c/deps/libpng-1.6.9/.libs \
+-L/c/deps/qrencode-3.4.3/.libs" \
 ./configure \
 --disable-upnp-default \
 --enable-debug \
@@ -237,5 +253,14 @@ $flag1 \
 $flag2 \
 $flag3 \
 $flag4 \
---with-protoc-bindir=/c/deps/protobuf-2.5.0/src \
---with-incompatible-bdb
+--with-qt-incdir=/c/Qt/5.2.1/include \
+--with-qt-libdir=/c/Qt/5.2.1/lib \
+--with-qt-bindir=/c/Qt/5.2.1/bin \
+--with-qt-plugindir=/c/Qt/5.2.1/plugins \
+--with-boost-libdir=/c/deps/boost_1_55_0/stage/lib \
+--with-boost-system=mgw48-mt-s-1_55 \
+--with-boost-filesystem=mgw48-mt-s-1_55 \
+--with-boost-program-options=mgw48-mt-s-1_55 \
+--with-boost-thread=mgw48-mt-s-1_55 \
+--with-boost-chrono=mgw48-mt-s-1_55 \
+--with-protoc-bindir=/c/deps/protobuf-2.5.0/src
