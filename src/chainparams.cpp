750ca8967f7fd431bf22b563aefa0dd8d12e9ab9
frank <2942612645@qq.com>
2015-04-30 17:04:55 +0800

    add seed node server
    Former-commit-id: f04dbf9bd145a2be655f9db27158a5b2544b7f5b

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -115,7 +115,7 @@ string initPubKey[] = { //
 //		"03ae28a4100145a4c354338c727a54800dc540069fa2f5fd5d4a1c80b4a35a1762"
 };
 unsigned int pnSeed[] = //
-		{0xa78a2879, 0x532d1fac};
+		{0xa78a2879, 0x532d1fac, 0x85a21c73};
 
 class CMainParams: public CBaseParams {
 public:
