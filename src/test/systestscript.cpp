d819ed31b7bd90b6fc35460ef79a986f21276036
frank <2942612645@qq.com>
2015-05-01 15:47:08 +0800

    fixed unit case sysScript_test/script_test 113 row bug
    Former-commit-id: 18eebbf15cc0a8e2b0e0ecea6cd562e6836591a8

--- src/test/systestscript.cpp
+++ src/test/systestscript.cpp
@@ -97,26 +97,26 @@ public:
 	void CheckSdk()
 	{
 		string param ="01";
-		Value resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param,10);
+		Value resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param, 10);
 		BOOST_CHECK(GetHashFromCreatedTx(resut,TxHash));
 		BOOST_CHECK(GenerateOneBlock());
 		uint256 hash(TxHash.c_str());
 		param ="02";
 		param += HexStr(hash);
 		string temp;
-		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param,10);
+		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param, 10);
 		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
 		BOOST_CHECK(GenerateOneBlock());
 
 		param ="03";
-		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param,10);
+		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param, 10, 10000000);
 		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
 		BOOST_CHECK(GenerateOneBlock());
 
 		param ="05";
 		param += HexStr(hash);
 
-		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param,10,10000000);
+		resut =CreateContractTx("010000000100", "e21rEzVwkPFQYfgxcg7xLp7DKeYrW4Fpoz", param, 10, 10000000);
 		BOOST_CHECK(GetHashFromCreatedTx(resut,temp));
 		BOOST_CHECK(GenerateOneBlock());
 	}
