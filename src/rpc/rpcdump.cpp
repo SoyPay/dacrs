3f047092619f3cd287535535cb6c198c576ff272
frank <2942612645@qq.com>
2015-07-10 21:31:43 +0800

    update version to 1.1.0.0

--- src/rpc/rpcdump.cpp
+++ src/rpc/rpcdump.cpp
@@ -239,8 +239,6 @@ Value dumpprivkey(const Array& params, bool fHelp)
     return reply;
 }
 
-
-
 Value dumpwallet(const Array& params, bool fHelp) {
 	if (fHelp || params.size() != 1)
 		throw runtime_error("dumpwallet \"filename\"\n"
