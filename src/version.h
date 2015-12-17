9ae7754fb1c30d82fc577fddf251c15603ef7ac4
frank <2942612645@qq.com>
2015-12-17 17:14:44 +0800

    更新版本至v1.1.2.1
    Signed-off-by: frank <2942612645@qq.com>

--- src/version.h
+++ src/version.h
@@ -32,7 +32,7 @@ static const int PROTOCOL_VERSION = 10007;
 static const int INIT_PROTO_VERSION = 10001;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 10007;
+static const int MIN_PEER_PROTO_VERSION = 10006;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
