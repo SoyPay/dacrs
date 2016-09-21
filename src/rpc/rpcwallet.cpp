6d056c05c3cf5b97b28e76dfd14522820c97fb79
xgc <xgc@163.com>
2016-09-21 11:32:53 +0800

    fix getassets rpc接口bug

--- src/rpc/rpcwallet.cpp
+++ src/rpc/rpcwallet.cpp
@@ -645,6 +645,7 @@ Value getassets(const Array& params, bool fHelp)
 			continue;
 		}
 
+		temp.get()->AutoMergeFreezeToFree(chainActive.Tip()->nHeight);
 		uint64_t freeValues = temp.get()->getllValues();
 		uint64_t freezeValues = temp.get()->GetAllFreezedValues();
 		totalassets += freeValues;
